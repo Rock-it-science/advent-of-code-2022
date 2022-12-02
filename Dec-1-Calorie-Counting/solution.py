7bc244f934d1a5e431c2c58b533fdcd7f9f7cbf5
wmcfarland <wmcfarland@helcim.com>
2022-12-02 13:04:18 -0700

    Day 1 part 2

--- Dec-1-Calorie-Counting/solution.py
+++ Dec-1-Calorie-Counting/solution.py
@@ -1,6 +1,8 @@
 lines = []
-with open('input.txt', 'r') as f:
+with open('Dec-1-Calorie-Counting/input.txt', 'r') as f:
     lines = f.readlines()
+
+all_elf_cals = [] # List of all elf calorie numbers for pt. 2
 
 max_elf_index = 1 # Elf with max calories
 max_elf_cals = 0  # Max elf calories
@@ -13,6 +15,7 @@ for line in lines:
         current_elf_cals += int(line)
     else:
         # End of elf
+        all_elf_cals.append(current_elf_cals)
         if current_elf_cals > max_elf_cals:
             max_elf_cals = current_elf_cals
             max_elf_index = current_elf_index
@@ -20,3 +23,8 @@ for line in lines:
         current_elf_cals = 0
 
 print(f'Elf number {max_elf_index} has the most calories with {max_elf_cals}')
+
+# Part 2: Compute top 3 elf calories total
+all_elf_cals.sort(reverse=True)
+top_3_cals_total = all_elf_cals[0] + all_elf_cals[1] + all_elf_cals[2]
+print(f'The top 3 elves are carrying {top_3_cals_total} calories')
